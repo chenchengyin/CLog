35b36eebcd7fcc431f93cab2377cc286e8cd853a
陈成印 <chenchengyin@cvte.com>
2019-10-12 15:14:03 +0800

    [modify]完善下demo

--- app/src/main/java/com/cvt/clog/MainActivity.kt
+++ app/src/main/java/com/cvt/clog/MainActivity.kt
@@ -3,7 +3,6 @@ package com.cvt.clog
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.cvt.library.clog.CLog
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,14 +13,12 @@ class MainActivity : AppCompatActivity() {
 
         val kotlinLogTest = KotlinLogTest()
         kotlinLogTest.testLog(applicationContext)
-//
         Log.e("分割线", "======以下是Java的调用========")
-        Log.e("分割线", "======以下是Java的调用========")
-        Log.e("分割线", "======以下是Java的调用========")
+
         val javaLogTest = JavaLogTest()
         javaLogTest.testLog(applicationContext)
 
-        CLog.clearLogFile()
-        CLog.release() //用了Log4aLogEngin时,程序退出要调用这个
+//        CLog.clearLogFile()
+//        CLog.release() //用了Log4aLogEngin时,程序退出要调用这个
     }
 }
